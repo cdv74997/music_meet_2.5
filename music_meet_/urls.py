a428225eb543dc99da60f7a1fe33851bc3103cbf
John <christian.vega.720@my.csun.edu>
2023-04-04 23:09:56 -0700

    Password Recovery Options Added
    Templating looks much better than default but can still use some improvements

--- music_meet_/urls.py
+++ music_meet_/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.contrib.auth import views as auth_views
+
 
 
 
@@ -10,6 +12,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
     path('api/', include('base.api.urls')),
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="reset_password.html"), name="reset_password"),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="reset_password_sent.html"), name="password_reset_done"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="reset.html"), name="password_reset_confirm"),
+    path('reset/password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="reset_password_complete.html"), name="password_reset_complete"),
+
 ]
 # setting a url and the file path will be this from set the url to getting the file from root
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
